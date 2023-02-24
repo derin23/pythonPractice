9b4f359ca6c8d4bc5c0e569bdcf752b201020ad2
derin <derinjoseph23@gmail.com>
2023-02-24 00:17:10 -0600

    day 6 was mostly on reeborg.ca

---
+++ day6/functions.py
@@ -0,0 +1,18 @@
+print("Hello")
+num_char = len("Hello")
+
+print(num_char)
+
+
+def my_function():
+    print("Hello")
+    print("Bye")
+
+
+my_function()
+
+num = 10
+
+while num > 0:
+    print(num)
+    num -= 1
